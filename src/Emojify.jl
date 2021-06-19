979fee4849035081329eba0206d9a98e9ba784d5
Ilia Kats <ilia-kats@gmx.net>
2021-06-20 01:23:35 +0200

    follow includes if we're emojifying files
    only works for literal incudes, include paths generated at runtime are
    not supported

--- src/Emojify.jl
+++ src/Emojify.jl
@@ -6,77 +6,101 @@ export emojify, emojify_string
 
 const emoji = Char.(0x1F400:0x1F6A6)
 
-function _emojify_string(str::AbstractString, out::IO)
-    emojis = shuffle(emoji)
-    emojiidx = [1]
+mutable struct EmojiEnv
+    emojis::Vector{Char}
+    emojiidx::Vector{UInt}
+    replacements::Dict{String, String}
+    basedir::String
+    outdir::Union{String, Nothing}
+end
 
+EmojiEnv(basedir::String, outdir::Union{String, Nothing}=nothing) = EmojiEnv(shuffle(emoji), [1], Dict{String, String}(), abspath(basedir), abspath(outdir))
+EmojiEnv(emojis::Vector{Char}, basedir::String, outdir::Union{String, Nothing}=nothing) = EmojiEnv(emojis, [1], Dict{String, String}(), abspath(basedir), abspath(outdir))
+
+function _replace(key::AbstractString, env::EmojiEnv)
+    if !haskey(env.replacements, key)
+        env.replacements[key] = String([env.emojis[i] for i in env.emojiidx])
+        cidx = length(env.emojiidx)
+        while cidx > 0
+            env.emojiidx[cidx] += 1
+            if env.emojiidx[cidx] > length(env.emojis)
+                env.emojiidx[cidx] = 1
+                cidx -= 1
+            else
+                break
+            end
+        end
+        if cidx == 0
+            push!(env.emojiidx, 1)
+        end
+    end
+end
+
+function _replace(key::CSTParser.EXPR, env::EmojiEnv)
+    name = CSTParser.valof(CSTParser.get_name(key))
+    if !isnothing(name)
+        _replace(name, env)
+    end
+end
+
+_isinclude(cst::CSTParser.EXPR) = CSTParser.iscall(cst) && CSTParser.valof(CSTParser.get_name(cst)) == "include" && CSTParser.isstringliteral(cst.args[2])
+
+function _emojify_string(str::AbstractString, out::IO, env::EmojiEnv, include_path::Union{AbstractString, Nothing}=nothing, errorstring="input")
     cst = CSTParser.parse(str, true)
     cu = codeunits(str)
 
     offset = 1
     stack = Vector{CSTParser.EXPR}()
-    replacements = Dict{String, String}()
-
-    function replace(key::AbstractString)
-        if !haskey(replacements, key)
-            replacements[key] = String([emojis[i] for i in emojiidx])
-            cidx = length(emojiidx)
-            while cidx > 0
-                emojiidx[cidx] += 1
-                if emojiidx[cidx] > length(emojis)
-                    emojiidx[cidx] = 1
-                    cidx -= 1
-                else
-                    break
-                end
-            end
-            if cidx == 0
-                push!(emojiidx, 1)
-            end
-        end
-    end
-
-    function replace(key::CSTParser.EXPR)
-        name = CSTParser.valof(CSTParser.get_name(key))
-        if !isnothing(name)
-            replace(name)
-        end
-    end
-
     push!(stack, cst)
+
     while length(stack) > 0
         cst = pop!(stack)
         if isnothing(cst.args)
-            if CSTParser.isidentifier(cst) && haskey(replacements, CSTParser.valof(cst))
-                cem = replacements[CSTParser.valof(cst)]
+            if CSTParser.isidentifier(cst) && haskey(env.replacements, CSTParser.valof(cst))
+                cem = env.replacements[CSTParser.valof(cst)]
                 write(out, cem)
                 if cst.fullspan > cst.span
                     write(out, cu[(offset + cst.span):(offset + cst.fullspan - 1)])
                 end
-            else
+            elseif cst.fullspan > 0
                 write(out, cu[offset:(offset + cst.fullspan - 1)])
+            elseif !isnothing(CSTParser.valof(cst))
+                write(out, CSTParser.valof(cst))
+            elseif CSTParser.headof(cst) === :errortoken
+                error("Syntax error in $errorstring")
             end
             offset += cst.fullspan
         else
             if CSTParser.defines_function(cst)
                 for sigpart in CSTParser.get_sig(cst)
-                    replace(CSTParser.get_arg_name(sigpart))
+                    _replace(CSTParser.get_arg_name(sigpart), env)
                 end
             elseif CSTParser.isparameters(cst)
                 for param in cst
-                    replace(CSTParser.get_arg_name(param))
+                    _replace(CSTParser.get_arg_name(param), env)
                 end
             elseif CSTParser.isassignment(cst)
-                replace(cst[1])
+                _replace(cst[1], env)
             elseif CSTParser.defines_datatype(cst)
-                replace(cst)
+                _replace(cst, env)
                 for c in cst
                     if CSTParser.headof(c) === :block # body of type definition
                         for cb in c
-                            replace(cb)
+                            _replace(cb, env)
                         end
                         break
                     end
+                end
+            elseif !isnothing(include_path) && _isinclude(cst)
+                includefile = CSTParser.valof(cst.args[2])
+                absp = isabspath(includefile)
+                if !absp
+                    includefile = joinpath(include_path, includefile)
+                end
+                newpath = _emojify_file(includefile, env)
+                if absp
+                    cst.args[2].val = newpath
+                    cst.args[2].fullspan = 0
                 end
             end
 
@@ -87,20 +111,30 @@ function _emojify_string(str::AbstractString, out::IO)
     end
 end
 
+function _emojify_file(file::AbstractString, env::EmojiEnv)
+    origin = read(file, String)
+
+    outdir = env.outdir
+    include_path, filename = splitdir(file)
+    subdir = include_path[length(env.basedir) + 1:end]
+    env.outdir = mkpath(joinpath(outdir, subdir))
+    outfile = joinpath(env.outdir, filename)
+    open(outfile, "w") do out
+        _emojify_string(origin, out, env, include_path, file)
+    end
+    env.outdir = outdir
+    return outfile
+end
+
 function emojify_string(str::AbstractString)
     out = IOBuffer(sizehint=sizeof(str))
-    _emojify_string(str, out)
+    _emojify_string(str, out, EmojiEnv())
     return String(take!(out))
 end
 
-function emojify(infile::AbstractString)
-    return emojify_string(read(infile, String))
-end
-
-function emojify(infile::AbstractString, outfile::AbstractString)
-    open(outfile, "w") do out
-        _emojify_string(read(infile, String), out)
-    end
+function emojify(infile::AbstractString, outdir::AbstractString)
+    _emojify_file(abspath(infile), EmojiEnv(dirname(infile), outdir))
+    nothing
 end
 
 end # module
